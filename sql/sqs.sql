f3aa1c92bfde4637a5b8582b89a05192f6fc27c0
jp <jp@blood.localdomain>
2021-03-02 18:01:06 +1000

    integrity constraints on sql

--- sql/sqs.sql
+++ sql/sqs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Mar 01, 2021 at 10:30 PM
+-- Generation Time: Mar 02, 2021 at 07:59 AM
 -- Server version: 5.7.33
--- PHP Version: 7.4.8
+-- PHP Version: 7.4.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,6 +32,15 @@ CREATE TABLE `class` (
   `name` varchar(92) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`id`, `name`) VALUES
+(1, 'webdev stage 1'),
+(2, 'web dev stage 2'),
+(3, 'webtech ');
+
 -- --------------------------------------------------------
 
 --
@@ -46,13 +55,6 @@ CREATE TABLE `logtable` (
   `response_code` int(11) NOT NULL,
   `ip_addr` varchar(39) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `logtable`
---
-
-INSERT INTO `logtable` (`id`, `datestamp`, `url`, `uid`, `response_code`, `ip_addr`) VALUES
-(1, '2021-03-01 03:34:47', '/api?action=showqueue', 1234123499, 200, '');
 
 -- --------------------------------------------------------
 
@@ -89,60 +91,30 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`student_NO`, `class_id`, `nick`, `pass`, `color`, `icon`, `theme`, `privilege`) VALUES
-(450338884, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(454920851, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(455846105, 0, 'nickname', 'password', '000000', 'icon-text', NULL, 1),
-(456787423, 0, 'nickname', 'password', '000000', 'icon-text', NULL, 1),
-(457120517, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(459118568, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(459193975, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(460796279, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(460931363, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(460950504, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(461417065, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(463563304, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(464874510, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(466974839, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(467551883, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(467621504, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(468299219, 0, NULL, NULL, NULL, NULL, NULL, 2),
-(468398359, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(469825145, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(470842368, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(470892907, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(470913303, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(470963201, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(470986402, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471011185, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471012861, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471130213, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471211710, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471452001, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471454793, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471457815, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471523347, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471528041, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471533155, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471539043, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471609755, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471614351, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471623027, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471628640, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471936466, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471952608, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471952718, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471967204, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471985206, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(471989981, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472003837, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472022858, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472055029, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472059610, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472062799, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472062922, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472077188, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472084492, 0, NULL, NULL, NULL, NULL, NULL, 0),
-(472086760, 0, NULL, NULL, NULL, NULL, NULL, 0);
+(460931363, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(461417065, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(465063493, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(467551883, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(468299219, 2, 'fragspawn', 'asdfasdf', NULL, NULL, NULL, 2),
+(470842368, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(470892907, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(470957194, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(470963201, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471011185, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471012861, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471243153, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471457815, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471523347, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471528041, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471533155, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471614351, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(471952718, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(472059610, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(472062799, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(472077188, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(472084492, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(472086760, 2, NULL, NULL, NULL, NULL, NULL, 0),
+(472487169, 2, NULL, NULL, NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -152,7 +124,7 @@ INSERT INTO `student` (`student_NO`, `class_id`, `nick`, `pass`, `color`, `icon`
 
 CREATE TABLE `suggestion` (
   `id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
+  `student_id` bigint(11) NOT NULL,
   `sggestion` varchar(1024) NOT NULL,
   `queue_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -191,9 +163,8 @@ ALTER TABLE `student`
 -- Indexes for table `suggestion`
 --
 ALTER TABLE `suggestion`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `queue_id` (`queue_id`),
-  ADD KEY `student_id` (`student_id`);
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `queue_id` (`queue_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -203,25 +174,7 @@ ALTER TABLE `suggestion`
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `logtable`
---
-ALTER TABLE `logtable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `queue`
---
-ALTER TABLE `queue`
-  MODIFY `queue_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `suggestion`
---
-ALTER TABLE `suggestion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -231,13 +184,20 @@ ALTER TABLE `suggestion`
 -- Constraints for table `queue`
 --
 ALTER TABLE `queue`
-  ADD CONSTRAINT `queue_ibfk_1` FOREIGN KEY (`student_NO`) REFERENCES `student` (`student_NO`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `queue_ibfk_1` FOREIGN KEY (`student_NO`) REFERENCES `student` (`student_NO`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `suggestion`
 --
 ALTER TABLE `suggestion`
-  ADD CONSTRAINT `suggestion_ibfk_1` FOREIGN KEY (`queue_id`) REFERENCES `queue` (`queue_ID`);
+  ADD CONSTRAINT `suggestion_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_NO`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `suggestion_ibfk_2` FOREIGN KEY (`queue_id`) REFERENCES `queue` (`queue_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
